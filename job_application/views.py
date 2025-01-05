00a6d6610eb35d4b228bd883c9e1f1760d9646bc
nmchugh17 <nmchu17@gmail.com>
2025-01-05 12:46:14 +0000

    created form model

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            date = form.cleaned_data['date']
+            occupation = form.cleaned_data['occupation']
+            print(first_name)
+        # form = Form(
+        #     first_name=first_name,
+        #     last_name=last_name,
+        #     email=email,
+        #     date=date_obj,
+        #     occupation=occupation
+        # )
+
+        # db.session.add(form)
+        # db.session.commit()
+
+        message_body = f"Thank you for your submission {first_name}." \
+                       f"Here is your data: \n{first_name}\n{last_name}\n{date}\n"
+        # message = Message(
+        #     subject='New Form Submission',
+        #     sender=app.config["MAIL_USERNAME"],
+        #     recipients=[email],
+        #     body=message_body
+        # )
+
+        # mail.send(message)
+
+        # flash(f"{first_name}, Your form was submitted successfully!", "success")
+
     return render(request, "index.html")
